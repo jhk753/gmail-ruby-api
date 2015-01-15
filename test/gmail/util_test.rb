0fd7b6df6f9168f87a4af37621df8513f190f868
Julien Hobeika <julien@wepopp.com>
2015-01-15 22:17:07 +0100

    make compatibility test for ruby 1.9

--- test/gmail/util_test.rb
+++ test/gmail/util_test.rb
@@ -5,11 +5,11 @@ module Gmail
   class UtilTest < Test::Unit::TestCase
 
     should "Symbolize Name should work properly" do
-      assert_equal(({test1: "test"}), Gmail::Util.symbolize_names({"test1": "test"}))
+      assert_equal(({:test1=> "test"}), Gmail::Util.symbolize_names({"test1"=>'test'}))
       m =Gmail::Message.new test_message
       assert_equal m, Gmail::Util.symbolize_names(m)
-      assert_equal(({test: {nested:[{coucou: "1", coucou2: "2"}]}}), Gmail::Util.symbolize_names({"test": {"nested":[{"coucou": "1", "coucou2": "2"}]}}))
-      assert_equal [{coucou: "1", coucou2: "2"}], Gmail::Util.symbolize_names([{"coucou": "1", "coucou2": "2"}])
+      assert_equal(({test: {nested:[{coucou: "1", coucou2: "2"}]}}), Gmail::Util.symbolize_names({"test"=>{"nested"=>[{"coucou"=>"1","coucou2"=>"2"}]}}))
+      assert_equal [{coucou: "1", coucou2: "2"}], Gmail::Util.symbolize_names([{"coucou"=>"1", "coucou2"=>"2"}])
     end
 
     #testing of convert_to_gmail_object is not necessary as it is tested through all other test files
